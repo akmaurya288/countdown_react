13fda15fa3eb21a956aacfa9afe28d686bbc79b6
akmaurya288 <akmaurya288@gmail.com>
2019-11-14 20:20:50 +0530

    Updates

---
+++ precache-manifest.40144fca9a3ee9c60676c863d5b39d27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d66130f5e0db574abdcca6c32455d9e",
+    "url": "/countdown_react/index.html"
+  },
+  {
+    "revision": "fde628d8711d12d906f2",
+    "url": "/countdown_react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1ef8e7c52f33df013de7",
+    "url": "/countdown_react/static/js/2.93a5995d.chunk.js"
+  },
+  {
+    "revision": "fde628d8711d12d906f2",
+    "url": "/countdown_react/static/js/main.e569bcb3.chunk.js"
+  },
+  {
+    "revision": "bba5d513e6056d78d7c8",
+    "url": "/countdown_react/static/js/runtime-main.65133e3f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/countdown_react/static/media/logo.25bf045c.svg"
+  }
+]);
